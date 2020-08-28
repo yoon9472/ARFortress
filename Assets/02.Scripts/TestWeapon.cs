02032f2a48a9d0f16eefba2b2c62e7038ceca666
meterslug4 <62926125+meterslug4@users.noreply.github.com>
2020-08-28 17:01:42 +0900

    로봇 조립 테스트
    로봇조립 테스트 매우 간단히 함

---
+++ Assets/02.Scripts/TestWeapon.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestWeapon : MonoBehaviour
+{
+    public Transform connectWeapon;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
