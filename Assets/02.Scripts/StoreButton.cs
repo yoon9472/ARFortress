89565073b0ea826df3d0823a68994a97c8d31215
JaeHoon-J <67031899+JaeHoon-J@users.noreply.github.com>
2020-09-11 14:43:59 +0900

    store
    test 상점씬

---
+++ Assets/02.Scripts/StoreButton.cs
@@ -0,0 +1,305 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using PlayFab;
+using PlayFab.ClientModels;
+
+public class StoreButton : MonoBehaviour
+{
+    int allCount = 1;
+    int weaponCount =1;
+    int upperBodyCount =1;
+    int lowerBodyCount =1;
+    
+    int whatItem = 1;
+
+    //점점 산으로 가는 느낌이다.
+    /*
+    public bool allItem = false;
+    public bool weaponItem = false;
+    public bool upperBodyItem = false;
+    public bool lowerBodyItem = false;
+    */
+    StoreInstantiate StIn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        WhatItemWillBeShowed(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void BacktoMainScene()
+    {
+        SceneManager.LoadScene("03.Lobby");
+        allCount =1;
+        weaponCount =1;
+        upperBodyCount =1;
+        lowerBodyCount =1;
+        whatItem =1;
+    }
+    //이 함수는 all/Weapon/UpperBody/LowerBody의 버튼을 눌렀을 때 보이는 아이템들을 변화할 수 있게 한다.
+    //그렇다면 처음이면 상관 없지만 만약 전에 생성 된 것이 있었다면? 어떻게 해야 할까?
+    public void WhatItemWillBeShowed(int k)
+    {
+        switch(k)
+        {
+            case 1:
+                StIn.ShowAllItem(allCount);
+            break;
+
+            case 2:
+                StIn.ShowWeaponItem(weaponCount);
+            break;
+
+            case 3:
+                StIn.ShowUpperBodyItem(upperBodyCount);
+            break;
+
+            case 4:
+                StIn.ShowLowerBodyItem(lowerBodyCount);
+            break;
+        }
+    }
+    //all 버튼을 눌렀을 때 모든 아이템들을 보일 수 있도록 한다.
+    public void AllButton()
+    {
+        whatItem = 1;
+        WhatItemWillBeShowed(whatItem);
+    }
+    //Weapon 버튼을 눌렀을 때 무기 아이템들을 보일 수 있도록 한다.
+    public void WeaponItem()
+    {
+        whatItem = 2;
+        WhatItemWillBeShowed(whatItem);
+    }
+    //UpperBody 버튼을 눌렀을 때 상체 아이템들을 보일 수 있도록 한다.
+    public void UpperBodyItem()
+    {
+        whatItem = 3;
+        WhatItemWillBeShowed(whatItem);
+    }
+    //LowerBody 버튼을 눌렀을 때 하체 아이템들을 보일 수 있도록 한다.
+    public void LowerBodyItem()
+    {
+        whatItem = 4;
+        WhatItemWillBeShowed(whatItem);
+    }
+    //이렇게 하는게 맞는지 의문이 든다. 아랫것은 그 아랫것으로 대체하기로 하자.
+    /*public void AllItemNext()
+    {
+        if (allItem ==false) return;
+
+        ++allCount;
+        if(allCount>7)return;
+
+        Debug.Log("allCount : "+ allCount);
+        StIn.ShowAllItem(allCount);
+    }
+    public void AllItemBefore()
+    {
+        if (allItem ==false) return;
+
+        --allCount;
+        if(allCount<1)return;
+
+        Debug.Log("allCount : "+ allCount);
+        StIn.ShowAllItem(allCount);
+    }
+    //무기
+    public void WeaponItemNext()
+    {
+        if (weaponItem ==false) return;
+
+        ++weaponCount;
+        if(weaponCount>3)return;
+
+        Debug.Log("weaponCount : "+ weaponCount);
+        StIn.ShowWeaponItem(weaponCount);
+    }
+    public void WeaponItemBefore()
+    {
+        if (weaponItem ==false) return;
+
+        --weaponCount;
+        if(weaponCount<1)return;
+
+        Debug.Log("weaponCount : "+ weaponCount);
+        StIn.ShowAllItem(weaponCount);
+    }
+    //상체
+    public void UpperBodyItemNext()
+    {
+        if (upperBodyItem ==false) return;
+
+        ++upperBodyCount;
+        if(upperBodyCount>3)return;
+
+        Debug.Log("upperBodyCount : "+ upperBodyCount);
+        StIn.ShowWeaponItem(upperBodyCount);
+    }
+    public void UpperBodyItemBefore()
+    {
+        if (upperBodyItem ==false) return;
+
+        --upperBodyCount;
+        if(upperBodyCount<1)return;
+
+        Debug.Log("upperBodyCount : "+ upperBodyCount);
+        StIn.ShowAllItem(upperBodyCount);
+    }
+    //하체
+    public void LowerBodyItemNext()
+    {
+        if (lowerBodyItem ==false) return;
+
+        ++lowerBodyCount;
+        if(lowerBodyCount>3)return;
+
+        Debug.Log("lowerBodyCount : "+ lowerBodyCount);
+        StIn.ShowWeaponItem(lowerBodyCount);
+    }
+    public void LowerBodyItemBefore()
+    {
+        if (lowerBodyItem ==false) return;
+
+        --lowerBodyCount;
+        if(lowerBodyCount<1)return;
+
+        Debug.Log("lowerBodyCount : "+ lowerBodyCount);
+        StIn.ShowAllItem(lowerBodyCount);
+    }*/
+    //오른쪽으로 넘어갈때!
+    public void PlusClick()
+    {
+        switch(whatItem)
+        {
+            case 1:
+                ++allCount;
+                if(allCount>7)return;
+
+                Debug.Log("allCount : "+ allCount);
+                StIn.ShowAllItem(allCount);
+            break;
+
+            case 2:
+                ++weaponCount;
+                if(weaponCount>3)return;
+
+                Debug.Log("weaponCount : "+ weaponCount);
+                StIn.ShowWeaponItem(weaponCount);
+            break;
+
+            case 3:
+                ++upperBodyCount;
+                if(upperBodyCount>3)return;
+
+                Debug.Log("upperBodyCount : "+ upperBodyCount);
+                StIn.ShowUpperBodyItem(upperBodyCount);
+            break;
+
+            case 4:
+                ++lowerBodyCount;
+                if(lowerBodyCount>3)return;
+
+                Debug.Log("lowerBodyCount : "+ lowerBodyCount);
+                StIn.ShowLowerBodyItem(lowerBodyCount);
+            break;
+        }
+    }
+    //왼쪽으로 넘어갈때!
+    public void MinusClick()
+    {
+        switch(whatItem)
+        {
+            case 1:
+                --allCount;
+                if(allCount<1)return;
+
+                Debug.Log("allCount : "+ allCount);
+                StIn.ShowAllItem(allCount);
+            break;
+
+            case 2:
+                --weaponCount;
+                if(weaponCount<1)return;
+
+                Debug.Log("weaponCount : "+ weaponCount);
+                StIn.ShowWeaponItem(weaponCount);
+            break;
+
+            case 3:
+                --upperBodyCount;
+                if(upperBodyCount<1)return;
+
+                Debug.Log("upperBodyCount : "+ upperBodyCount);
+                StIn.ShowUpperBodyItem(upperBodyCount);
+            break;
+
+            case 4:
+                --lowerBodyCount;
+                if(lowerBodyCount<1)return;
+
+                Debug.Log("lowerBodyCount : "+ lowerBodyCount);
+                StIn.ShowLowerBodyItem(lowerBodyCount);
+            break;
+        }
+    }
+    public void BuyItem()
+    {
+        /* 이것들은 예시이다!!! 맨 아래것 빼고 어디서 처리를 해야 하는 것일까?
+        void GetVcStore() 이것은 store를 가져와서 사용자들에게 표시하는 것이다.
+        {
+            var primaryCatalogName = "TestCatalog-001"; // In your game, this should just be a constant matching your primary catalog
+            var storeId = "Potion Store"; // In your game, this should be a constant for a permanent store, or retrieved from titleData for a time-sensitive store
+            var request = new GetStoreItemsRequest
+            {
+                CatalogVersion = primaryCatalogName,
+                StoreId = storeId
+            };
+            PlayFabClientAPI.GetStoreItems(request, LogSuccess, LogFailure);
+        }
+
+        void DefinePurchase() 이것은 물건을 살 때 하는 것 같은데.. 뭐지??
+        {
+            var primaryCatalogName = "TestCatalog-001"; // In your game, this should just be a constant matching your primary catalog
+            var storeId = "Potion Store"; // At this point in the process, it's just maintaining the same storeId used above
+            var request = new StartPurchaseRequest
+            {
+                CatalogVersion = primaryCatalogName,
+                StoreId = storeId,
+                Items = new List<ItemPurchaseRequest> {
+                    // The presence of these lines are based on the results from GetStoreItems, and user selection - Yours will be more generic
+                    new ItemPurchaseRequest { ItemId = "Small Health Potion", Quantity = 20,},
+                    new ItemPurchaseRequest { ItemId = "Medium Health Potion", Quantity = 100,},
+                    new ItemPurchaseRequest { ItemId = "Large Health Potion", Quantity = 2,},
+                }
+            };
+            PlayFabClientAPI.StartPurchase(request, result => { Debug.Log("Purchase started: " + result.OrderId); }, LogFailure);
+        }
+
+        //아래 것은 물 건 살 때 통화에 대한 것 같다. 맞는 지는 모르겠지만.
+        void DefinePaymentCurrency(string orderId, string currencyKey, string providerName)
+        {
+            var request =new PayForPurchaseRequest {
+                OrderId = orderId, // orderId comes from StartPurchase above
+                Currency = currencyKey, // User defines which currency they wish to use to pay for this purchase (all items must have a defined/non-zero cost in this currency)
+                ProviderName = providerName // providerName comes from the PaymentOptions in the result from StartPurchase above.
+            };
+            PlayFabClientAPI.PayForPurchase(request, LogSuccess, LogFailure);
+        }
+
+        // Unity/C# 유니티에서 작업하는 것은 이것 뿐인가?? 맞나? 물건 살 때 마지막으로 하는 것인가?
+        void FinishPurchase(string orderId)
+        {
+            var request = new ConfirmPurchaseRequest { OrderId = orderId };
+            PlayFabClientAPI.ConfirmPurchase(request, LogSuccess, LogFailure);
+        }
+        */
+    }
+}
---
+++ Assets/02.Scripts/StoreInstantiate.cs
@@ -0,0 +1,231 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using PlayFab;
+using PlayFab.ClientModels;
+
+public class StoreInstantiate : MonoBehaviour
+{
+    GameObject pos;
+    public GameObject pos1;
+    public GameObject pos2;
+    public GameObject pos3;
+
+    public Text title1;
+    public Text title2;
+    public Text title3;
+
+    public Text introduction1;
+    public Text introduction2;
+    public Text introduction3;
+
+    Text cost1;
+    Text cost2;
+    Text cost3;
+
+    public GameObject costparent1;
+    public GameObject costparent2;
+    public GameObject costparent3;
+
+    public GameObject tank;
+
+    public GameObject weapon1;
+    public GameObject weapon2;
+    public GameObject weapon3;
+    public GameObject weapon4;
+    public GameObject weapon5;
+    public GameObject weapon6;
+    public GameObject weapon7;
+
+
+    NetWork network;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cost1 = costparent1.GetComponentInChildren<Text>();
+        cost2 = costparent2.GetComponentInChildren<Text>();
+        cost3 = costparent3.GetComponentInChildren<Text>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    //얘를 껐다 킬 수 있는 것일까?
+    GameObject item1;
+    GameObject item2;
+    GameObject item3;
+    //음 하는데 밖으로 빼고 싶다...
+    public void ShowAllItem(int count)
+    {
+        switch (count)
+        {
+            case 1 :
+                
+            break;
+
+            case 2 :
+
+            break;
+        }
+        //이것으로 할지
+        for (int i =0 ; i < 7; i++ )
+        {
+            if(count == i)
+            {
+                // for (int j =0; j < 3; j++)
+                // {
+                //     Instantiate(tank+i, pos+j; Quaternion.identity);
+                // }요건 안된다고 하니 패스
+            }//이것으로 할지 고민중 하지만 둘 다 엄청난 노가다가 필요할 것이라는 것은 똑같음.
+        }
+    }
+
+    public void ShowWeaponItem(int count)
+    {
+        switch (count)
+        {
+            case 1 :
+                if(item1 != null) 
+                {
+                    Destroy(item1);
+                    Destroy(item2);
+                    Destroy(item3);//요 3개를 배열로 만들어서 for문으로 돌려도 될 것 같다.
+                }
+                //1번부터 생성시킬 것임
+                title1.text = "아모건";
+                item1 = Instantiate(weapon1, pos1.transform.position, Quaternion.identity);
+                introduction1.text =  "무게 : 35 " +"\r\n"
+                                    + "공격 : 120 " +"\r\n"
+                                    + "사정거리 : 15";
+                cost1.text = "0";
+
+                //2번 생성
+                title2.text = "머신건";
+                item2 = Instantiate(weapon2, pos.transform.position, Quaternion.identity);
+                introduction2.text =  "무게 : 40 " +"\r\n"
+                                    + "공격 : 140 " +"\r\n"
+                                    + "사정거리 : 15";
+                cost2.text = "0";
+
+                //3번 생성
+                title3.text =" 핸드캐넌";
+                item3 = Instantiate(weapon3, pos.transform.position, Quaternion.identity);
+                introduction3.text =  "무게 : 50 " +"\r\n"
+                                    + "공격 : 160 " +"\r\n"
+                                    + "사정거리 : 20";
+                cost3.text = "300";
+            break;
+
+            case 2 :
+                if(item1 != null) 
+                {
+                    Destroy(item1);
+                    if(item2 == null) 
+                    break;//이거 맞으려나?
+                    Destroy(item2);
+                    Destroy(item3);//요 3개를 배열로 만들어서 for문으로 돌려도 될 것 같다.
+                }
+                //4번 생성
+                title1.text = "팔랑스";
+                item1 =Instantiate(weapon4, pos1.transform.position, Quaternion.identity);
+                introduction1.text =  "무게 : 70 " +"\r\n"
+                                    + "공격 : 200 " +"\r\n"
+                                    + "사정거리 : 35" +"\r\n"
+                                    + "방어력 무시 : 20" +"\r\n"
+                                    + "체력비례 데미지 10% ";
+                cost1.text = "2000";
+
+                //5번 생성
+                title2.text = "배럴";
+                item2 = Instantiate(weapon5, pos.transform.position, Quaternion.identity);
+                introduction2.text =  "무게 : 50 " +"\r\n"
+                                    + "공격 : 200 " +"\r\n"
+                                    + "사정거리 : 20" +"\r\n"
+                                    + "폭발 범위 증가";
+                cost2.text = "3000";
+
+                //6번 생성
+                title3.text ="데미시스";
+                item3 = Instantiate(weapon6, pos.transform.position, Quaternion.identity);
+                introduction3.text =  "무게 : 50 " +"\r\n"
+                                    + "공격 : 160 " +"\r\n"
+                                    + "사정거리 : 20" +"\r\n"
+                                    + "피격시 체력 비례 지속대미지 5%";
+                cost3.text = "4000";            
+            break;
+
+            case 3:
+                if(item1 != null) 
+                {
+                    Destroy(item1);
+                    Destroy(item2);
+                    Destroy(item3);//만약 2,3번에 null인데 destroy를 실행 시킨다면?
+                }
+                //7번 생성
+                title1.text = "바주카";
+                item1 =Instantiate(weapon7, pos1.transform.position, Quaternion.identity);
+                introduction1.text =  "무게 : 55 " +"\r\n"
+                                    + "공격 : 200 " +"\r\n"
+                                    + "사정거리 : 22" +"\r\n"
+                                    + "폭발범위 증가";
+                cost1.text = "5000";
+
+                //0번 생성
+                title2.text = " ";
+                item2 = null;
+                introduction2.text = " ";
+                cost2.text = " ";
+
+                //0번 생성
+                title3.text =" ";
+                item3 = null;
+                introduction3.text = " ";
+            break;
+        }
+        if(count == 1)
+        {
+            
+        }
+    }
+    
+    public void ShowUpperBodyItem(int count)
+    {
+        switch (count)
+        {
+            case 1 :
+
+            break;
+
+            case 2 :
+
+            break;
+        }
+
+        if(count == 1)
+        {
+
+        }
+    }
+    
+    public void ShowLowerBodyItem(int count)
+    {
+        switch (count)
+        {
+            case 1 :
+
+            break;
+
+            case 2 :
+
+            break;
+        }
+
+        if(count == 1)
+        {
+
+        }
+    }
+}
