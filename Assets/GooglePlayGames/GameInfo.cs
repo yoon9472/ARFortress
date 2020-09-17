e875ae2e705da9592b3976a6bbf2ba338c366803
meterslug4 <62926125+meterslug4@users.noreply.github.com>
2020-09-17 17:52:41 +0900

    구글로그인 성공
    플레이팹 구글로그인 연동성공
    로컬 프로젝트에 키스토어 파일 추가하고 구글 api 콘솔에도 키스토어 추가해줘야
    로컬빌드한거 구글로그인 가능함

--- Assets/GooglePlayGames/GameInfo.cs
+++ Assets/GooglePlayGames/GameInfo.cs
@@ -36,9 +36,9 @@ namespace GooglePlayGames {
         private const string UnescapedWebClientId = "WEB_CLIENTID";
         private const string UnescapedNearbyServiceId = "NEARBY_SERVICE_ID";
 
-        public const string ApplicationId = "450680152283"; // Filled in automatically
+        public const string ApplicationId = "330683352662"; // Filled in automatically
         public const string IosClientId = "__IOS_CLIENTID__"; // Filled in automatically
-        public const string WebClientId = "450680152283-nk1ohkuducv32621tmduhsje0jstuola.apps.googleusercontent.com"; // Filled in automatically
+        public const string WebClientId = "330683352662-6okk6pqm03ventuteera52nj6g1a9t38.apps.googleusercontent.com"; // Filled in automatically
         public const string NearbyConnectionServiceId = "";
 
         public static bool ApplicationIdInitialized() {
