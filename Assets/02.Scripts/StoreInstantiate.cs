0660c4baef434b1329f29e4231a8566d5e8af859
yoon9472 <57215037+yoon9472@users.noreply.github.com>
2020-09-17 17:53:45 +0900

    복구맨
    으아

--- Assets/02.Scripts/StoreInstantiate.cs
+++ Assets/02.Scripts/StoreInstantiate.cs
@@ -38,26 +38,20 @@ public class StoreInstantiate : MonoBehaviour
     public GameObject weapon6;
     public GameObject weapon7;
 
-    string store="Store";
+
+    NetWork network;
     // Start is called before the first frame update
     void Start()
     {
         cost1 = costparent1.GetComponentInChildren<Text>();
         cost2 = costparent2.GetComponentInChildren<Text>();
         cost3 = costparent3.GetComponentInChildren<Text>();
-        StoreItemList();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-    }
-    //여기서 playfab의 아이템 리스트를 가지고 오려고 한다.
-    //저거로 가져오려고 하지만 어떻게 리스트에 저장을 할까?읭?? 어떤 형식으로 저장이 될 것인가?
-    public void StoreItemList()
-    {
-        NetWork.Get.GetCatalogItem(store);
-        
     }
     //얘를 껐다 킬 수 있는 것일까?
     GameObject item1;
@@ -98,7 +92,7 @@ public class StoreInstantiate : MonoBehaviour
                 {
                     Destroy(item1);
                     Destroy(item2);
-                    Destroy(item3);
+                    Destroy(item3);//요 3개를 배열로 만들어서 for문으로 돌려도 될 것 같다.
                 }
                 //1번부터 생성시킬 것임
                 title1.text = "아모건";
@@ -129,11 +123,10 @@ public class StoreInstantiate : MonoBehaviour
                 if(item1 != null) 
                 {
                     Destroy(item1);
-                    //만약 2,3번에 null인데 destroy를 실행 시킨다면? 다음과 같이 하면 pass하려나..?
                     if(item2 == null) 
                     break;//이거 맞으려나?
                     Destroy(item2);
-                    Destroy(item3);
+                    Destroy(item3);//요 3개를 배열로 만들어서 for문으로 돌려도 될 것 같다.
                 }
                 //4번 생성
                 title1.text = "팔랑스";
@@ -169,7 +162,7 @@ public class StoreInstantiate : MonoBehaviour
                 {
                     Destroy(item1);
                     Destroy(item2);
-                    Destroy(item3);
+                    Destroy(item3);//만약 2,3번에 null인데 destroy를 실행 시킨다면?
                 }
                 //7번 생성
                 title1.text = "바주카";
