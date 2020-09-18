2e3bdb3cfc51212e8a754bf13a442300602f0bf0
yoon9472 <57215037+yoon9472@users.noreply.github.com>
2020-09-18 19:13:11 +0900

    수정중맨
    수정중임

---
+++ Assets/02.Scripts/Lab/Item.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item 
+{
+    public enum ItemType
+    {
+        
+    }
+    public class Items
+    {
+        public string itemName;
+        public int damage;
+        public int itemPrice;
+        public int hp;
+        public string itemDesc;
+        //아이템이미지
+        public Sprite itemImage;
+        //아이템 3d 오브젝트
+        public GameObject itemP;
+        //아이템 형태( 탑형,인간형, 탱크형 ,, 팔형, 탑형, 어깨형 등)
+        public string itemType;
+    }
+    public Item(string _itemName, int _itemValue, int _itemPrice, string _itemDesc, Sprite _itemImage, GameObject _itemP)
+    {
+
+    }
+    public Item()
+    {
+
+    }
+}
