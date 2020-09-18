81a3b34a8c70c02987d3fe6e51827b71980297b1
yoon9472 <57215037+yoon9472@users.noreply.github.com>
2020-09-18 13:59:44 +0900

    Resources 에 부품추가
    ㅁㄷㄹ

---
+++ Assets/02.Scripts/Lab/RobotManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotManager : MonoBehaviour
+{
+    public List<GameObject> leg = new List<GameObject>();
+    public List<GameObject> bodie = new List<GameObject>();
+    public List<GameObject> weapon = new List<GameObject>();
+
+
+    
+}
