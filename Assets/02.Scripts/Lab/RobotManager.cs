def0fe52601735be91d8010106017e2f45baf78b
yoon9472 <57215037+yoon9472@users.noreply.github.com>
2020-09-18 19:16:48 +0900

    테스트씬 뺴고 올리기
    머지용

--- Assets/02.Scripts/Lab/RobotManager.cs
+++ Assets/02.Scripts/Lab/RobotManager.cs
@@ -8,26 +8,6 @@ public class RobotManager : MonoBehaviour
     public List<GameObject> bodie = new List<GameObject>();
     public List<GameObject> weapon = new List<GameObject>();
 
-    public Transform createSpot;
-    public void Start()
-    {
-   
-       
-    }
 
-    public void LegSpiderCreate()
-    {
-        Vector3 createPos = createSpot.position;
-
-        if (GameObject.FindGameObjectsWithTag("Leg") !=null)
-        {
-          GameObject obj = Instantiate(Resources.Load("03.Leg/Spider[4legs]") as GameObject, createPos, Quaternion.identity);
-            obj.transform.localScale = new Vector3(5, 5, 5);
-        }
-        else
-        {
-            print("다리(하체) 부품이 이미 존재합니다.");
-        }
-
-    }
+    
 }
