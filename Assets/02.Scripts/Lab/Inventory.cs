da15b692fd927f8fe75a85423cfef29937056116
yoon9472 <57215037+yoon9472@users.noreply.github.com>
2020-09-18 13:49:03 +0900

    Labui
    조립실 메뉴판까지 완성

---
+++ Assets/02.Scripts/Lab/Inventory.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public List<GameObject> inventory = new List<GameObject>();
+
+    public GameObject currItem;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Inven0Open()
+    {
+        if(currItem != null)
+        {
+            currItem.SetActive(false);
+            currItem = null;
+        }
+        inventory[0].SetActive(true);
+        currItem = inventory[0] ;
+    }
+    public void Inven1Open()
+    {
+        if (currItem != null)
+        {
+            currItem.SetActive(false);
+            currItem = null;
+        }
+        inventory[1].SetActive(true);
+        currItem = inventory[1];
+    }
+    public void Inven2Open()
+    {
+        if (currItem != null)
+        {
+            currItem.SetActive(false);
+            currItem = null;
+        }
+        inventory[2].SetActive(true);
+        currItem = inventory[2];
+    }
+    public void Inven0Close()
+    {
+        inventory[0].SetActive(false);
+    }
+    public void Inven1Close()
+    {
+        inventory[1].SetActive(false);
+    }
+    public void Inven2Close()
+    {
+        inventory[2].SetActive(false);
+    }
+
+}
