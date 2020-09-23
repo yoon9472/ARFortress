34f38b5410acc5f1b657e8ab5ac2ba76b4f0ebe7
JaeHoon-J <67031899+JaeHoon-J@users.noreply.github.com>
2020-09-23 19:10:56 +0900

    loading test
    씬만들어서 test 만들어봄

---
+++ Assets/02.Scripts/TestLoading.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestLoading : MonoBehaviour
+{
+    int speed = 20;
+    int movespeed = 20;
+    public GameObject robot;
+    public GameObject white; 
+    public GameObject final;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        robot.transform.Rotate(Vector3.up, speed*Time.deltaTime, Space.World);
+        if(white.transform.position.y>22.4)
+        {
+            white.transform.position=new Vector3(0,-17,68);
+        }
+        white.transform.Translate(Vector3.up*Time.deltaTime*movespeed, Space.World);
+    }
+}
