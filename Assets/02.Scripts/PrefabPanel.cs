2b99fce73c7d6bc20a296c5f4fb906ed3e98e131
JaeHoon-J <67031899+JaeHoon-J@users.noreply.github.com>
2020-09-24 15:30:29 +0900

    보유중인 아이템 상점에서 못사게, 그리고 다이아몬드 없앰
    인트로 이름 바꿈

--- Assets/02.Scripts/PrefabPanel.cs
+++ Assets/02.Scripts/PrefabPanel.cs
@@ -9,12 +9,14 @@ public class PrefabPanel : MonoBehaviour
     public Text introduction;
     public GameObject buttonText;
     Text cost;
+    public GameObject buttonImage;
     public string displayname;
     public string description;
     public string itemcost;
     public string itemId;
     public int price;
     public Image image;
+    bool canBuy = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +24,16 @@ public class PrefabPanel : MonoBehaviour
         title.text = displayname;
         introduction.text = description;
         cost.text = itemcost;
+        for(int i=0; i<GameManager.Get.ownedItem_List.Count;i++)
+        {
+            if(GameManager.Get.ownedItem_List[i] == displayname)
+            {
+                canBuy = false;
+                cost.text = "Owned";
+                buttonImage.gameObject.SetActive(false);
+            }
+        }
+
     }
 
     // Update is called once per frame
@@ -30,6 +42,7 @@ public class PrefabPanel : MonoBehaviour
     }
     public void Buy()
     {
+        if(canBuy == false) return;
         Debug.Log("나 이거 살꺼다!");
         NetWork.Get.BuyItem("Store", itemId ,"GD", price);
         
